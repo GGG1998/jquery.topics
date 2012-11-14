f5a718b8453d14417fd1ea9dbf013268fa48af8c
Riga <marcelrieger@me.com>
2012-11-14 16:52:24 +0100

    Fix github link.

--- jquery.Topics.js
+++ jquery.Topics.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery Topics Plugin v0.1
- * https://github.com/riga/jquery.History
+ * https://github.com/riga/jquery.Topics
  *
  * Copyright 2012, Marcel Rieger
  * Dual licensed under the MIT or GPL Version 3 licenses.
