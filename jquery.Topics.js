3fd301735007ef1b223ccce45140dab2bad53a39
Riga <marcelrieger@me.com>
2012-11-14 16:51:14 +0100

    Add license part. Update jquery.Topics.min.js.

--- jquery.Topics.js
+++ jquery.Topics.js
@@ -1,11 +1,19 @@
-/*
- * Lightweight pub/sub mechanism based on jQuery.Callback
+/*!
+ * jQuery Topics Plugin v0.1
+ * https://github.com/riga/jquery.History
+ *
+ * Copyright 2012, Marcel Rieger
+ * Dual licensed under the MIT or GPL Version 3 licenses.
+ * http://www.opensource.org/licenses/mit-license
+ * http://www.opensource.org/licenses/GPL-3.0
+ *
+ * Based upon a pub/sub example by The jQuery Foundation:
+ * http://api.jquery.com/jQuery.Callbacks/
  */
 
 var _topics = {};
 
 jQuery.Topic = function( /*String|Integer*/ id, /*String*/ flags ) {
-	// instantiated on every call
 	
 	var self = id && _topics[ id ];
 	
